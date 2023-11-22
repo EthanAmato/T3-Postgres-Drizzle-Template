d0aee6f1a64a8e0f0d71df413e1befe73567c83c
EthanAmato <ethannamato@gmail.com>
2023-11-22 15:21:38 -0500

    changed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function Home() {
-  // const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   return (
